5a426942cc6bf5397d434edfa42a736ac85e2b7c
sokolovdm <naraxiss@gmail.com>
2017-06-02 23:11:53 +0300

    Add files via upload

---
+++ exam/js/castom.js
@@ -0,0 +1,26 @@
+//Анимация WOW+CSS
+new WOW().init(); 
+
+//Плавный скрол
+$(document).ready(function(){
+		$("a.header__btn").on("click", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1000);
+		});
+	});
+
+
+$(document).ready(function(){
+  $("#carousel").owlCarousel({
+  	items: 1,
+  	singleItem: true,
+  	autoplay: 5000,
+  	nav: true,
+  	dots: true,
+  	dotsEach: true,
+  	loop: true,
+  	navText:""
+  });
+});
