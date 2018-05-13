96114763355822f8a4c1c3d9b2bcf639431fb962
dm <naraxiss@gmail.com>
2018-05-13 23:06:15 +0300

    fixes

--- js/core.bundle.js
+++ js/core.bundle.js
@@ -12,14 +12,55 @@ $('document').ready(function () {
         };
     }
 
-
-    $('.form__button--submite-main, .form__button--submite-example').on('click', function(){
-        $(this).closest('.form').addClass('ok');
-    })
-
     $('.form__button--back-main').on('click', function(){
         $(this).closest('.form').removeClass('ok');
     })
+
+    $('._submite').on('click', function () {
+       var $this = $(this),
+           form = $this.closest('.form'),
+           input = form.find('input'),
+           textarea = form.find('textarea');
+
+       if(input.val() === ''){
+           input.closest('.input').addClass('error')
+       } else {
+           input.closest('.input').removeClass('error')
+       }
+
+       if(!input.closest('.input').hasClass('error')){
+
+
+           $.ajax({
+               type: "POST",
+               url: form.attr('action'),
+               data: form.serialize()
+               /* statusCode: {
+                    404: function() {
+                        setTimeout(function() {
+                           form.removeClass('loading');
+                           form.addClass('ok');
+                           textarea.val('');
+                           input.val('');
+                       }, 1)
+                    }
+                }*/
+           }).done(function() {
+               setTimeout(function() {
+                   form.removeClass('loading');
+                   form.addClass('ok');
+                   textarea.val('');
+                   input.val('');
+               }, 1)
+           });
+       }
+
+    });
+
+
+
+
+
 
 });
 
