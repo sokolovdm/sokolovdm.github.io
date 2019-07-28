01e298fda327d0612e9552762c29410d39e0c294
Dmitriy <naraxiss@gmail.com>
2019-07-29 00:37:56 +0400

    new project blockchain

--- _js/core.js
+++ _js/core.js
@@ -1,66 +1,7 @@
-$('document').ready(function () {
+document.addEventListener("DOMContentLoaded", ready);
 
-    function __lI(el) {
-        var imgP = new Image();
-
-        imgP.src = el.dataset.src;
-        imgP.onload = function () {
-            $(el).attr('src', $(el).data('src'));
-            $(el).addClass('loaded');
-
-        };
+function ready(){
+    if (window.innerWidth >= 1152){
+        document.getElementById('main').style.marginBottom = document.getElementById('footer').getBoundingClientRect().height  + 'px';
     }
-
-    $('.form__button--back-main').on('click', function(){
-        $(this).closest('.form').removeClass('ok');
-    })
-
-    $('._submite').on('click', function () {
-       var $this = $(this),
-           form = $this.closest('.form'),
-           input = form.find('input'),
-           textarea = form.find('textarea');
-
-       if(input.val() === ''){
-           input.closest('.input').addClass('error')
-       } else {
-           input.closest('.input').removeClass('error')
-       }
-
-       if(!input.closest('.input').hasClass('error')){
-
-           form.addClass('loading');
-           $.ajax({
-               type: "POST",
-               url: form.attr('action'),
-               data: form.serialize()
-               /* statusCode: {
-                    404: function() {
-                        setTimeout(function() {
-                           form.removeClass('loading');
-                           form.addClass('ok');
-                           textarea.val('');
-                           input.val('');
-                       }, 1)
-                    }
-                }*/
-           }).done(function() {
-               setTimeout(function() {
-                   form.removeClass('loading');
-                   form.addClass('ok');
-                   textarea.val('');
-                   input.val('');
-               }, 1)
-           });
-       }
-
-    });
-
-
-
-
-
-
-});
-
-
+}
--- js/core.bundle.js
+++ js/core.bundle.js
@@ -1,69 +1,9 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-$('document').ready(function () {
+document.addEventListener("DOMContentLoaded", ready);
 
-    function __lI(el) {
-        var imgP = new Image();
-
-        imgP.src = el.dataset.src;
-        imgP.onload = function () {
-            $(el).attr('src', $(el).data('src'));
-            $(el).addClass('loaded');
-
-        };
+function ready(){
+    if (window.innerWidth >= 1152){
+        document.getElementById('main').style.marginBottom = document.getElementById('footer').getBoundingClientRect().height  + 'px';
     }
-
-    $('.form__button--back-main').on('click', function(){
-        $(this).closest('.form').removeClass('ok');
-    })
-
-    $('._submite').on('click', function () {
-       var $this = $(this),
-           form = $this.closest('.form'),
-           input = form.find('input'),
-           textarea = form.find('textarea');
-
-       if(input.val() === ''){
-           input.closest('.input').addClass('error')
-       } else {
-           input.closest('.input').removeClass('error')
-       }
-
-       if(!input.closest('.input').hasClass('error')){
-
-           form.addClass('loading');
-           $.ajax({
-               type: "POST",
-               url: form.attr('action'),
-               data: form.serialize()
-               /* statusCode: {
-                    404: function() {
-                        setTimeout(function() {
-                           form.removeClass('loading');
-                           form.addClass('ok');
-                           textarea.val('');
-                           input.val('');
-                       }, 1)
-                    }
-                }*/
-           }).done(function() {
-               setTimeout(function() {
-                   form.removeClass('loading');
-                   form.addClass('ok');
-                   textarea.val('');
-                   input.val('');
-               }, 1)
-           });
-       }
-
-    });
-
-
-
-
-
-
-});
-
-
-
+}
 },{}]},{},[1]);
