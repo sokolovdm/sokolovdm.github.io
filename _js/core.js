19cfc85a80d4aec2f47dabae8ef8a41f2a860c5b
Dmitriy <naraxiss@gmail.com>
2019-10-25 00:46:52 +0400

    init new project

--- _js/core.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    console.log(111);
-    $(".main a").on("click", function (event) {
-        console.log(this);
-        event.preventDefault();
-        $('body,html').animate({scrollTop: $($(this).attr('href')).offset().top}, 1000);
-    });
-});
-
