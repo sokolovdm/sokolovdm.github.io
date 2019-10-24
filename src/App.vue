07f0f28e498be15fca8e6d08509bfcbeff818ad7
Dmitriy <naraxiss@gmail.com>
2019-10-25 01:59:18 +0400

    11

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  include ../src/libs/bemto/bemto.pug
+  - set_bemto_settings({ prefix: 'a94a8fe5ccb19ba61c-', modifier: '--' })
+
+  <div id="app">
+    <div id="nav">
+      +b.alo
+        +e.ne-alo
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
